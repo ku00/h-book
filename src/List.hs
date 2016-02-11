5b18c174830f1a8187316cde7d07c52c0b288603
ku00 <skentaro36@gmail.com>
2016-02-12 00:10:53 +0900

    finished Chapter-1

---
+++ src/List.hs
@@ -0,0 +1,7 @@
+-- 1.5 リスト内包表記
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
