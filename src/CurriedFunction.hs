8c3cb2f7dd14effc09e887a78e6da0fc3e178232
ku00 <skentaro36@gmail.com>
2016-03-01 07:48:52 +0900

    Chapter5-3

--- src/CurriedFunction.hs
+++ src/CurriedFunction.hs
@@ -24,3 +24,26 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f y x = f x y
+
+-- 5.3 関数プログラマの道具箱
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x       = x : filter' p xs
+    | otherwise = filter' p xs
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) =
+    let smallerOrEqual = filter' (<= x) xs
+        larger = filter' (> x) xs
+    in quicksort' smallerOrEqual ++ [x] ++ quicksort' larger
+
+largestDivisible :: Integer
+largestDivisible = head (filter p [100000,99999..])
+    where p x = x `mod` 3829 == 0
