99964be606cf3d2fbdb82a7f51d147f441ac8c02
ku00 <skentaro36@gmail.com>
2016-03-06 18:23:42 +0900

    finished Chapter-5

--- src/CurriedFunction.hs
+++ src/CurriedFunction.hs
@@ -62,3 +62,53 @@ numLongChains = length (filter isLong (map chain [1..100]))
 
 numLongChains' :: Int
 numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+addThree' :: Int -> Int -> Int -> Int
+addThree' = \x -> \y -> \z -> x + y + z
+
+flip'' :: (a -> b -> c) -> b -> a -> c
+flip'' f = \x y -> f y x
+
+-- 5.5 畳み込み、見込みアリ!
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+sum'' :: (Num a) => [a] -> a
+sum'' = foldl (+) 0
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = foldr (\x acc -> f x : acc) [] xs
+
+map''' :: (a -> b) -> [a] -> [b]
+map''' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+elem'' :: (Eq a) => a -> [a] -> Bool
+elem'' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' = foldl1 max
+
+reverse'' :: [a] -> [a]
+reverse'' = foldl (\acc x -> x : acc) []
+
+reverse''' :: [a] -> [a]
+reverse''' = foldl (flip (:)) []
+
+product'' :: (Num a) => [a] -> a
+product'' = foldl (*) 1
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (< 1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
+squareSum :: Integer
+squareSum = sum . takeWhile (<10000) . filter odd $ map (^2) [1..]
