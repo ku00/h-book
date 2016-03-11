235f8d44b3847e10bdb1a83f85487198f781b2f7
ku00 <skentaro36@gmail.com>
2016-03-11 09:57:58 +0900

    wipp Chapter-6.2

--- src/ModulePractice.hs
+++ src/ModulePractice.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Char
 
 -- 6.1 モジュールをインポートする
 numUniques :: (Eq a) => [a] -> Int
@@ -7,3 +8,12 @@ numUniques = length . nub
 -- 6.2 標準モジュールの関数で問題を解く
 wordNums :: String -> [(String, Int)]
 wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+encode :: Int -> String  -> String
+encode offset msg = map (\c -> chr $ ord c + offset) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
