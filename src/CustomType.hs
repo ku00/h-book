1294a0ad5a61739f4b4840848b1f42efbbfd4cf7
ku00 <skentaro36@gmail.com>
2016-03-20 22:53:10 +0900

    finished Chapter-7

--- src/CustomType.hs
+++ src/CustomType.hs
@@ -1,23 +1,26 @@
 -- 7.2 形づくる
 
 import Shapes
+import qualified Data.Map as Map
 
 createCircle :: IO ()
 createCircle = print $ nudge (baseCircle 30) 10 20
 
 -- 7.3 レコード構文
 
-data Person = Person { firstName :: String
-                     , lastName :: String
-                     , age :: Int
-                     , height :: Float
-                     , phoneNumber :: String
-                     , flavor :: String } deriving (Show)
+data Person = Person
+    { firstName :: String
+    , lastName :: String
+    , age :: Int
+    , height :: Float
+    , phoneNumber :: String
+    , flavor :: String } deriving (Show)
 
-data Car = Car { company :: String
-               , model :: String
-               , year :: Int
-               } deriving (Show)
+data Car = Car
+    { company :: String
+    , model :: String
+    , year :: Int
+    } deriving (Show)
 
 -- 7.4 型引数
 
@@ -39,7 +42,7 @@ vmult :: (Num a) => Vector a -> a -> Vector a
 -- 7.5 インスタンスの自動導出
 
 data Person' = Person' { firstName' :: String, lastName' :: String, age' :: Int}
-                deriving (Eq, Show, Read)
+    deriving (Eq, Show, Read)
 
 mikeD = Person' { firstName' = "Michael", lastName' = "Diamond", age' = 43}
 adRock = Person' { firstName' = "Adam", lastName' = "Horovitz", age' = 41}
@@ -48,4 +51,131 @@ mca = Person' { firstName' = "Adam", lastName' = "Yauch", age' = 44}
 mysteryDude = "Person' { firstName' = \"Michael\"" ++ ", lastName' = \"Diamond\"" ++ ", age' = 43}"
 
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
-            deriving (Eq, Ord, Show, Read, Bounded, Enum)
+    deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+-- 7.6 型シノニム
+
+phoneBook''' =
+    [("betty", "555-2938")
+    ,("betty", "342-2492")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("patsy", "943-2929")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ,("penny", "555-2111")
+    ]
+
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+
+data LockerState = Taken | Free deriving (Show, Eq)
+type Code = String
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+        Nothing            -> Left $ "Locker " ++ show lockerNumber ++ " doesn't exist!"
+        Just (state, code) -> if state /= Taken
+                                then Right code
+                                else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList
+    [ (100, (Taken, "ZD39I"))
+    , (101, (Free,  "JAH3I"))
+    , (103, (Free,  "IWSA9"))
+    , (105, (Free,  "QOTSA"))
+    , (109, (Taken, "893JJ"))
+    , (110, (Taken, "99292"))
+    ]
+
+-- 7.7 再帰的なデータ構造
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
+
+-- 7.8 型クラス 中級編
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red       = True
+    Green == Green   = True
+    Yellow == Yellow = True
+    _ == _           = False
+
+instance Show TrafficLight where
+    show Red    = "Red light"
+    show Yellow = "Yellow light"
+    show Green  = "Green light"
+
+-- 7.9 YesとNoの型クラス
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _  = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing  = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _         = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _   = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult =
+    if yesno yesnoVal
+        then yesResult
+        else noResult
+
+-- 7.10 Functor型クラス
+
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
