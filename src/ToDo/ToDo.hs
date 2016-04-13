563f6ded733caa73429d5fac5681cf067d5b63fe
ku00 <skentaro36@gmail.com>
2016-04-13 23:24:27 +0900

    fix ToDo.hs
    - add import Control.Exception for bracketOnError
    - add dir

--- src/ToDo/ToDo.hs
+++ src/ToDo/ToDo.hs
@@ -2,6 +2,9 @@ import System.Environment
 import System.Directory
 import System.IO
 import Data.List
+import Control.Exception
+
+dir = "src/files_for_stream/"
 
 dispatch :: String -> [String] -> IO ()
 dispatch "add" = add
@@ -9,34 +12,34 @@ dispatch "view" = view
 dispatch "remove" = remove
 
 main = do
-      (command:argList) <- getArgs
-      dispatch command argList
+    (command:argList) <- getArgs
+    dispatch command argList
 
 add :: [String] -> IO ()
-add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+add [fileName, todoItem] = appendFile (dir ++ fileName) (todoItem ++ "\n")
 
 view :: [String] -> IO ()
 view [fileName] = do
-    contents <- readFile fileName
+    contents <- readFile (dir ++ fileName)
     let todoTasks = lines contents
         numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
     putStr $ unlines numberedTasks
 
 remove :: [String] -> IO ()
 remove [fileName, numberString] = do
-    contents <- readFile fileName
+    contents <- readFile (dir ++ fileName)
     let todoTasks = lines contents
         numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
     putStrLn "These are your TO-DO items:"
     mapM_ putStrLn numberedTasks
     let number = read numberString
         newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
-    bracketOnError (openTempFile "." "temp")
+    bracketOnError (openTempFile dir "temp")
         (\(tempName, tempHandle) -> do
             hClose tempHandle
             removeFile tempName)
         (\(tempName, tempHandle) -> do
             hPutStr tempHandle newTodoItems
             hClose tempHandle
-            removeFile "todo.txt"
-            renameFile tempName "todo.txt")
+            removeFile (dir ++ "todo.txt")
+            renameFile tempName (dir ++ "todo.txt"))
