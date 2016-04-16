0804c85c461f7327014fddb59b4e59cd778f797c
ku00 <skentaro36@gmail.com>
2016-04-16 23:17:28 +0900

    finished Chapter-9.6

---
+++ src/RandomString.hs
@@ -0,0 +1,11 @@
+import System.Random
+
+-- main = do
+--     gen <- getStdGen
+--     putStrLn $ take 20 (randomRs ('a', 'z') gen)
+
+main = do
+    gen <- getStdGen
+    putStrLn $ take 20 (randomRs ('a', 'z') gen)
+    gen' <- newStdGen
+    putStrLn $ take 20 (randomRs ('a', 'z') gen')
