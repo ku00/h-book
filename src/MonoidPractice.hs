12a22fe0fd62c5c7f9064654d17afe93afcddd9a
ku00 <skentaro36@gmail.com>
2016-05-03 23:48:18 +0900

    finished Chapter-12

--- src/MonoidPractice.hs
+++ src/MonoidPractice.hs
@@ -1,4 +1,5 @@
 import Data.Monoid
+import qualified Data.Foldable as F
 
 lengthCompare :: String -> String -> Ordering
 lengthCompare x y = let a = length x `compare` length y
@@ -13,3 +14,22 @@ lengthCompare'' x y = (length x `compare` length y) `mappend`
                       (vowels x `compare` vowels y) `mappend`
                       (x `compare` y)
     where vowels = length . filter (`elem` "aeiou")
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a)
+    deriving (Show)
+
+instance F.Foldable Tree where
+    foldMap f EmptyTree = mempty
+    foldMap f (Node x l r) = F.foldMap f l `mappend`
+                             f x           `mappend`
+                             F.foldMap f r
+
+testTree = Node 5
+            (Node 3
+              (Node 1 EmptyTree EmptyTree)
+              (Node 6 EmptyTree EmptyTree)
+            )
+            (Node 9
+              (Node 8 EmptyTree EmptyTree)
+              (Node 10 EmptyTree EmptyTree)
+            )
