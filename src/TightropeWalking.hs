38c950c83df1cf2c153e92e7aaf375b5874b14a4
ku00 <skentaro36@gmail.com>
2016-05-05 15:23:09 +0900

    wip Chapter-13.5

---
+++ src/TightropeWalking.hs
@@ -0,0 +1,17 @@
+type Birds = Int
+type Pole = (Birds, Birds)
+
+x -: f = f x
+
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise                    = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    | otherwise                    = Nothing
+
+banana :: Pole -> Maybe Pole
+banana _ = Nothing
