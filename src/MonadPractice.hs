c1a0aa6c54859669014ae60863cbdf6c130cac93
ku00 <skentaro36@gmail.com>
2016-05-07 20:30:55 +0900

    wip2 Chapter-13.6

--- src/MonadPractice.hs
+++ src/MonadPractice.hs
@@ -1,3 +1,5 @@
+import Control.Monad
+
 applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
 applyMaybe Nothing f = Nothing
 applyMaybe (Just x) f = f x
@@ -26,4 +28,16 @@ justH = do
 wopwop :: Maybe Char
 wopwop = do
     (x:xs) <- Just ""
+    return x
+
+listOfTuples :: [(Int, Char)]
+listOfTuples = do
+    n <- [1,2]
+    ch <- ['a','b']
+    return (n,ch)
+
+sevensOnly :: [Int]
+sevensOnly = do
+    x <- [1..50]
+    guard ('7' `elem` show x)
     return x
